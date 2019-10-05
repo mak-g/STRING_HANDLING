a25f1c5438a7896ef91595a52a96ed7e5e2992e1
mak-g <mayank.goyal_cs18@gla.a.c.in>
2019-10-05 21:11:52 +0530

    question3 completed

--- src/question3/Question3.java
+++ src/question3/Question3.java
@@ -3,5 +3,13 @@ package question3;
 import java.util.Scanner;
 
 public class Question3 {
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine().trim();
+        String s = "aeiou";
+        for (int i = 0; i < s.length(); i++) {
+            name = name.replace(s.charAt(i), '*');
+        }
+        System.out.println(name);
+    }
 }
