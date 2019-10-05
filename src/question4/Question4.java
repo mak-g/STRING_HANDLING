83cbb0c158ba5ff833fb5c7e21169b77280b88f7
mak-g <mayank.goyal_cs18@gla.a.c.in>
2019-10-05 21:18:58 +0530

    question4 completed

--- src/question4/Question4.java
+++ src/question4/Question4.java
@@ -1,5 +1,28 @@
 package question4;
 
-public class Question4 {
+import java.util.Scanner;
 
+public class Question4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String inputString = sc.nextLine().trim();
+        String s1 = "";
+        String s2 = inputString.toLowerCase();
+        int vowelCounter = 0;
+        for (int i = 0; i <= s2.length() - 1; i++) {
+            if (s2.charAt(i) == 'a' || s2.charAt(i) == 'e' || s2.charAt(i) == 'i' || s2.charAt(i) == 'o' || s2.charAt(i) == 'u') {
+                vowelCounter += 1;
+                if (vowelCounter % 3 == 1) {
+                    s1 += "*";
+                } else if (vowelCounter % 3 == 2) {
+                    s1 += "^";
+                } else {
+                    s1 += "!";
+                }
+            } else {
+                s1 += inputString.charAt(i);
+            }
+        }
+        System.out.println(s1);
+    }
 }
