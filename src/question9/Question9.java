4026b2053e7bb263b1f8f38624736478af7feda7
mak-g <mayank.goyal_cs18@gla.a.c.in>
2019-10-05 21:43:43 +0530

    question9 completed

--- src/question9/Question9.java
+++ src/question9/Question9.java
@@ -1,5 +1,26 @@
 package question9;
 
-public class Question9 {
+import java.util.Scanner;
 
+public class Question9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine().trim();
+        int a = 0;
+        int g = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (((int) s.charAt(i)) >= 65 && ((int) s.charAt(i)) <= 90) {
+                int j = s.charAt(i);
+                a += j;
+            } else {
+                int k = s.charAt(i);
+                g += k;
+            }
+        }
+        if ((a - g) > 0) {
+            System.out.println("absolute weight =" + (a - g));
+        } else {
+            System.out.println("absolute weight =" + Math.abs(a - g));
+        }
+    }
 }
