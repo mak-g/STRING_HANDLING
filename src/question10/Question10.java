f89cf7c3c09299a19d4ac52e9e6de879b85d1bc8
mak-g <mayank.goyal_cs18@gla.a.c.in>
2019-10-05 21:46:20 +0530

    question10 completed

--- src/question10/Question10.java
+++ src/question10/Question10.java
@@ -1,5 +1,11 @@
 package question10;
 
+import java.util.Scanner;
 public class Question10 {
-
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String inputString = s.nextLine().trim();
+        char ch = s.next().charAt(0);
+        System.out.println("Index is =" + inputString.indexOf(ch));
+    }
 }
