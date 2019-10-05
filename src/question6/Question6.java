3cef320dd49e355c0d120898aa8de4f55a15860e
mak-g <mayank.goyal_cs18@gla.a.c.in>
2019-10-05 21:29:11 +0530

    question6 completed

--- src/question6/Question6.java
+++ src/question6/Question6.java
@@ -1,5 +1,10 @@
 package question6;
 
+import java.util.Scanner;
 public class Question6 {
-
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String upper = s.nextLine().trim();
+        System.out.println(upper.toUpperCase());
+    }
 }
