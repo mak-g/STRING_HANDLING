bce19405cf56a16bf21957698c6f686d1f2a80bc
mak-g <mayank.goyal_cs18@gla.a.c.in>
2019-10-05 21:37:34 +0530

    question8 completed

--- src/question8/Question8.java
+++ src/question8/Question8.java
@@ -1,5 +1,18 @@
 package question8;
 
-public class Question8 {
+import java.util.Scanner;
 
+public class Question8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine().trim();
+        int a = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (((int) s.charAt(i)) >= 65 && ((int) s.charAt(i)) <= 90) {
+                int j = s.charAt(i);
+                a += j;
+            }
+        }
+        System.out.println("total weight =" + a);
+    }
 }
