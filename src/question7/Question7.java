acf23661f655c56cb2b26dc414d0fdb1cbab6d2f
mak-g <mayank.goyal_cs18@gla.a.c.in>
2019-10-05 21:33:51 +0530

    question7 completed

--- src/question7/Question7.java
+++ src/question7/Question7.java
@@ -3,5 +3,14 @@ package question7;
 import java.util.Scanner;
 
 public class Question7 {
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine().trim();
+        int a = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int j = s.charAt(i);
+            a += j;
+        }
+        System.out.println("total weight =" + a);
+    }
 }
