9b6a6cbfda2169196c2e900c9d78bcbe54832ab6
mak-g <mayank.goyal_cs18@gla.a.c.in>
2019-10-05 20:53:48 +0530

    Question 1 completed

--- src/question1/Question1.java
+++ src/question1/Question1.java
@@ -1,4 +1,20 @@
 package question1;
 
+import java.util.Scanner;
+
 public class Question1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String input = s.nextLine().trim();
+        int a = input.length();
+        System.out.print("index:");
+        for (int i = 0; i < a; i++) {
+            System.out.print("\t" + i);
+        }
+        System.out.println();
+        System.out.print("chars:");
+        for (int i = 0; i < a; i++) {
+            System.out.print("\t" + input.charAt(i));
+        }
+    }
 }
