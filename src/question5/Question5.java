84fd39fa9fbc7a36040e7f25c1903f8a5441dd42
mak-g <mayank.goyal_cs18@gla.a.c.in>
2019-10-05 21:27:25 +0530

    question5 completed

--- src/question5/Question5.java
+++ src/question5/Question5.java
@@ -1,5 +1,15 @@
 package question5;
 
-public class Question5 {
+import java.util.Scanner;
 
+
+public class Question5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String inputString = sc.nextLine().trim();
+        String subString = sc.nextLine();
+        String replaceString = sc.nextLine();
+        inputString = inputString.replaceAll(subString, replaceString);
+        System.out.println(inputString);
+    }
 }
