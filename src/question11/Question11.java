db90b971768f58dca1fc86c227f28485647bfd17
mak-g <mayank.goyal_cs18@gla.a.c.in>
2019-10-05 21:52:15 +0530

    question11 completed

--- src/question11/Question11.java
+++ src/question11/Question11.java
@@ -3,5 +3,14 @@ package question11;
 import java.util.Scanner;
 
 public class Question11 {
-
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String inputString = s.nextLine().trim();
+        char ch = s.next().charAt(0);
+        for (int i = 0; i < inputString.length(); i++) {
+            if (inputString.charAt(i) == ch) {
+                System.out.println(i);
+            }
+        }
+    }
 }
